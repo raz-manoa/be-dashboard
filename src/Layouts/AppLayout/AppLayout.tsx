f6b1e9ff1fd25fd7a5c0e847ef3f566738ccdc66
Manoa <cmanoa92@gmail.com>
2022-10-06 09:59:06 +0300

    Header title

--- src/Layouts/AppLayout/AppLayout.tsx
+++ src/Layouts/AppLayout/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Layout } from "antd";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { memo } from "react";
 import { AppLayoutContext } from "./AppLayoutContext";
 import Sidebar from "@/Components/Common/Sidebar/Sidebar";
 import Header from "@/Components/Common/Header/Header";
 
 const AppLayout = () => {
+  let { state } = useLocation();
+
   return (
     <AppLayoutContext.Provider value={{}}>
       <Layout style={{ minHeight: "100vh" }}>
@@ -16,7 +18,7 @@ const AppLayout = () => {
         <Layout.Content
           style={{ marginLeft: "20%", padding: "0 32px", marginTop: 95 }}
         >
-          <Header title="Styleguide" />
+          <Header title={state ? state.headerLabel || "" : ""} />
           <Outlet />
         </Layout.Content>
       </Layout>
