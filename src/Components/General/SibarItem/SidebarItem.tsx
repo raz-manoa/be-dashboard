79def2f3b16ab912c40b8379df26e57be7d11752
Manoa <cmanoa92@gmail.com>
2022-10-07 18:06:30 +0300

    login components

--- src/Components/General/SibarItem/SidebarItem.tsx
+++ src/Components/General/SibarItem/SidebarItem.tsx
@@ -1,10 +1,6 @@
 import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import "./SidebarItem.scss";
-=======
 import styles from "./SidebarItem.module.scss";
->>>>>>> 7d0bf4404901294031be0c3d7c6b40a12b24e334
 interface SidebarItemProps {
   to: string;
   label: string;
@@ -14,9 +10,6 @@ interface SidebarItemProps {
 export default function SidebarItem(props: SidebarItemProps) {
   const { to, label, children } = props;
   return (
-<<<<<<< HEAD
-    <Link to={to} className="sidebar-item">
-=======
     <Link
       to={to}
       state={{
@@ -24,7 +17,6 @@ export default function SidebarItem(props: SidebarItemProps) {
       }}
       className={styles.sidebarItem}
     >
->>>>>>> 7d0bf4404901294031be0c3d7c6b40a12b24e334
       {children}
       <span>{label}</span>
     </Link>
--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import LoginImg from "@/Assets/login__img.jpg";
+import styles from "./LoginPage.module.scss";
 const LoginPage = () => {
   const handleLogin = () => {
     localStorage.setItem("a", "logged");
   };
   return (
-    <div>
-    
+    <div className={styles.login}>
+      <img src={LoginImg} alt="login__img" className={styles.login__img} />
+      <div className={styles.login__content}></div>
       <button onClick={handleLogin}>Fake login</button>
     </div>
   );
