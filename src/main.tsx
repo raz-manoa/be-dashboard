24b3739d027426f48c0e8ab385672eca0cc09e0a
Manoa Razanajatovo <cmanoa92@gmail.com>
2022-10-07 09:44:40 +0300

    add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.css";
+import "./fonts.scss";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
