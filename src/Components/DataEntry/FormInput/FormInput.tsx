b3cf2a0707d2c2c7406e8e8febe0540f2cc5a111
Manoa Razanajatovo <cmanoa92@gmail.com>
2022-10-10 13:09:45 +0300

    Fix styleguid

--- src/Components/DataEntry/FormInput/FormInput.tsx
+++ src/Components/DataEntry/FormInput/FormInput.tsx
@@ -1,30 +1,48 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Input, { InputProps } from "antd/lib/input";
 import { UserOutlined } from "@ant-design/icons";
 import styles from "./FormInput.module.scss";
+import Form, { Rule } from "antd/es/form";
+import { NamePath } from "antd/es/form/interface";
+import Icon from "@/Components/General/Icon/Icon";
 interface FormInputProps extends InputProps {
-  icon?: ReactNode;
+  icon?: string;
   label?: string;
   color?: string;
+  rules?: Rule[];
 }
 
 export default function FormInput(props: FormInputProps) {
-  const { placeholder, type, icon, label, color = "red", ...rest } = props;
+  const {
+    placeholder,
+    type,
+    icon,
+    label,
+    color = "red",
+    name,
+    rules,
+    className = "",
+    ...rest
+  } = props;
   return (
-    <div className={`${styles.input}`}>
+    <Form.Item
+      className={`${styles.input} ${className}`}
+      name={name}
+      rules={rules}
+    >
       <label htmlFor="" className={styles.label}>
         {label}
       </label>
       <Input
         size="large"
         placeholder={placeholder}
-        prefix={icon}
+        prefix={icon && <Icon icon={icon} />}
         type={type}
         className={`${styles.input__content} ${
           styles[`input__content--${color}`]
         }`}
         {...rest}
       />
-    </div>
+    </Form.Item>
   );
 }
