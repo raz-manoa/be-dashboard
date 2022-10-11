b71745960cae06bfc4cc50d5a7b09ed275c2f12f
Manoa <cmanoa92@gmail.com>
2022-10-11 21:18:57 +0300

    alert styling

--- src/Pages/AppPage/SavingsPage/SavingsPage.tsx
+++ src/Pages/AppPage/SavingsPage/SavingsPage.tsx
@@ -1,12 +1,21 @@
+import Alert from "antd/es/alert";
 import React from "react";
 import styles from "./SavingPage.module.scss";
 import SavingPageAdd from "./SavingPageAdd/SavingPageAdd";
 import SavingPageOffering from "./SavingPageOffering/SavingPageOffering";
 import SavingPageTable from "./SavingPageTable/SavingPageTable";
 
+const showAlert = false;
 const SavingsPage = () => {
   return (
     <section className="mt-6">
+      {showAlert && (
+        <Alert
+          message="Insufficient USD funds."
+          type="error"
+          className="mb-8"
+        />
+      )}
       <div className="grid grid-cols-2 gap-5">
         <SavingPageOffering />
         <SavingPageAdd />
