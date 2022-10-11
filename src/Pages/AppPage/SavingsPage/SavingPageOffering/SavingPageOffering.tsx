6fda98706ee2dba15c527854bd702022014e25bf
Manoa <cmanoa92@gmail.com>
2022-10-11 19:28:10 +0300

    add saving page

---
+++ src/Pages/AppPage/SavingsPage/SavingPageOffering/SavingPageOffering.tsx
@@ -0,0 +1,10 @@
+import TitleCard from "@/Components/Display/TitleCard/TitleCard";
+import React from "react";
+
+export default function SavingPageOffering() {
+  return (
+    <div>
+      <TitleCard title="5% Savings Offering" subtitle="Balances" />
+    </div>
+  );
+}
--- src/Pages/AppPage/SavingsPage/SavingsPage.tsx
+++ src/Pages/AppPage/SavingsPage/SavingsPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./SavingPage.module.scss";
+import SavingPageOffering from "./SavingPageOffering/SavingPageOffering";
 
 const SavingsPage = () => {
-  return <div>SavingsPage</div>;
+  return (
+    <section>
+      <div className="grid grid-cols-2 gap-5">
+        <SavingPageOffering />
+      </div>
+      <div></div>
+    </section>
+  );
 };
 
 export default SavingsPage;
