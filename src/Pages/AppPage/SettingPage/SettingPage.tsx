a6ffc5e093222a749c16f6db2f41fa4f5e92c6c2
Manoa <cmanoa92@gmail.com>
2022-10-11 14:57:33 +0300

    finish card setting

--- src/Pages/AppPage/SettingPage/SettingPage.tsx
+++ src/Pages/AppPage/SettingPage/SettingPage.tsx
@@ -1,6 +1,39 @@
 import Card from "@/Components/Display/Card/Card";
+import Text from "@/Components/General/Text/Text";
+import styles from "./SettingPage.module.scss";
 import React from "react";
 
-export default function SettingPage() {
-  return <Card>SettingPage</Card>;
-}
+const SettingPage = () => {
+  const data = [
+    {
+      user: "John Smith",
+      company: "johnsmith@company.com",
+    },
+    {
+      user: "Jane Doe",
+      company: "johndoe@company.com",
+    },
+    {
+      user: "Tim Armstrong",
+      company: "timarmstrong@company.com",
+    },
+  ];
+  return (
+    <Card className={styles.card}>
+      <Text tag="h2" type="h2" size={19} className={styles.card__title}>
+        Access
+      </Text>
+      {data.map((d, index) => (
+        <div className={styles.card__list} key={`d-${index}`}>
+          <Text tag="span" type="span" variant="grey">
+            {d.user}
+          </Text>
+          <Text tag="p" type="p" variant="red" className={styles.company}>
+            {d.company}
+          </Text>
+        </div>
+      ))}
+    </Card>
+  );
+};
+export default SettingPage;
