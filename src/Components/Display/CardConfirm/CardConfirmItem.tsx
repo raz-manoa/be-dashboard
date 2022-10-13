b9a3d2967163446ee006416a2c35518f48b50fe7
Manoa <cmanoa92@gmail.com>
2022-10-13 12:43:20 +0300

    add event props in Card condirm

--- src/Components/Display/CardConfirm/CardConfirmItem.tsx
+++ src/Components/Display/CardConfirm/CardConfirmItem.tsx
@@ -1,21 +1,31 @@
 import Text, { TextProps } from "@/Components/General/Text/Text";
 import styles from "./CardConfirm.module.scss";
 import React from "react";
+import Icon from "@/Components/General/Icon/Icon";
 interface CardModalItemProps {
   color: TextProps["variant"];
   label: string;
   value: string;
+  icon?: string;
+  onClick?(e: React.MouseEvent): void;
 }
 export default function CardConfirmItem(props: CardModalItemProps) {
-  const { color, label, value } = props;
+  const { color, label, value, icon = "", onClick } = props;
   return (
     <div className={styles.cardModal__list}>
       <Text tag="span" variant="grey" type="span">
         {label}
       </Text>
-      <Text tag="h3" variant={color} type="h3">
-        {value}
-      </Text>
+      <div className={styles.cardModal__value}>
+        <Text tag="h3" variant={color} type="h3">
+          {value}
+        </Text>
+        {icon == "" && (
+          <span onClick={onClick}>
+            <Icon icon="icon" color="red" />
+          </span>
+        )}
+      </div>
     </div>
   );
 }
