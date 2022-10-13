1ca9847d36165028f32237b936a00135a1340b0c
Manoa <cmanoa92@gmail.com>
2022-10-13 14:57:22 +0300

    top_up page

--- src/Components/Display/CardConfirm/CardConfirmItem.tsx
+++ src/Components/Display/CardConfirm/CardConfirmItem.tsx
@@ -3,21 +3,27 @@ import styles from "./CardConfirm.module.scss";
 import React from "react";
 import Icon from "@/Components/General/Icon/Icon";
 interface CardModalItemProps {
-  color: TextProps["variant"];
+  color?: TextProps["variant"];
   label: string;
   value: string;
   icon?: string;
   onClick?(e: React.MouseEvent): void;
 }
 export default function CardConfirmItem(props: CardModalItemProps) {
-  const { color, label, value, icon = "", onClick } = props;
+  const { color = "black", label, value, icon = "", onClick } = props;
   return (
     <div className={styles.cardModal__list}>
-      <Text tag="span" variant="grey" type="span">
+      <Text
+        tag="span"
+        variant="grey"
+        type="span"
+        size={12}
+        className={styles.cardModal__label}
+      >
         {label}
       </Text>
       <div className={styles.cardModal__value}>
-        <Text tag="h3" variant={color} type="h3">
+        <Text tag="h3" variant={color} type="h3" size={12}>
           {value}
         </Text>
         {icon != "" && (
--- src/Pages/AppPage/TopUpPage/TopUpPage.tsx
+++ src/Pages/AppPage/TopUpPage/TopUpPage.tsx
@@ -1,7 +1,89 @@
+import Card from "@/Components/Display/Card/Card";
+import Text from "@/Components/General/Text/Text";
 import React from "react";
-
+import TopUpPageNav from "./TopUpPageNav/TopUpPageNav";
+import styles from "./TopUpPage.module.scss";
+import CardConfirmItem from "@/Components/Display/CardConfirm/CardConfirmItem";
+const moneyData = [
+  {
+    label: "USD (Mauritius)",
+    path: "#",
+  },
+  {
+    label: "MUR (Mauritius)",
+    path: "#",
+  },
+  {
+    label: "ZAR (Mauritius)",
+    path: "#",
+  },
+  {
+    label: "EUR (Mauritius)",
+    path: "#",
+  },
+  {
+    label: "EUR (SEPA)",
+    path: "#",
+  },
+];
+const data = [
+  {
+    label: "Currency",
+    value: "USD",
+  },
+  {
+    label: "Beneficiary Name",
+    value: "Be Mobile Limited",
+  },
+  {
+    label: "Beneficiary Address",
+    value:
+      "Level 2, Alexander House, Silicon Avenue,Cibercity, Ebene, Mauritius ",
+  },
+  {
+    label: "Bank Name",
+    value: "The Mauritius Commercial Bank Ltd",
+  },
+  {
+    label: "Bank Address",
+    value: "9-15 Sir William Newton Street, Port Louis",
+  },
+  {
+    label: "Bank Country",
+    value: "Mauritius",
+  },
+  {
+    label: "Account Number",
+    value: "000449876447656",
+    icon: "bank-transfert",
+  },
+  {
+    label: "IBAN",
+    value: "MU38MCBL876165318323290823132",
+    icon: "bank-transfert",
+  },
+];
 const TopUpPage = () => {
-  return <div>TopUpPage</div>;
+  return (
+    <Card className="common__card">
+      <Text tag="p" type="p" className={styles.txt__header} variant="grey">
+        To top up, please use the account information below to transfer funds:
+      </Text>
+      <div className={styles.card__nav__list}>
+        {moneyData.map((m, id) => (
+          <TopUpPageNav label={m.label} to={m.path} key={`m-${id}`} />
+        ))}
+      </div>
+      {data.map((d, id) => (
+        <CardConfirmItem
+          label={d.label}
+          value={d.value}
+          icon={d.icon}
+          key={`d-${id}`}
+        />
+      ))}
+    </Card>
+  );
 };
 
 export default TopUpPage;
