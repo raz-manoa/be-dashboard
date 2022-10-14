dcb3dcbed87ba5ab529d56ad026a45259f30af1f
Manoa <cmanoa92@gmail.com>
2022-10-14 15:37:05 +0300

    title company name and BE ID

--- src/Components/Common/Sidebar/Sidebar.tsx
+++ src/Components/Common/Sidebar/Sidebar.tsx
@@ -3,10 +3,19 @@ import Icon from "@/Components/General/Icon/Icon";
 import SidebarItem from "@/Components/General/SibarItem/SidebarItem";
 import styles from "./Sidebar.module.scss";
 import sidebarRoutes from "@/Routes/sidebarRoutes";
+import Text from "@/Components/General/Text/Text";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
+      <div className={styles.sidebar__header}>
+        <Text tag="h2" type="h2" variant="white" size={19}>
+          Company Name
+        </Text>
+        <Text tag="p" type="p" size={16}>
+          Be ID: BE007054
+        </Text>
+      </div>
       <div className={styles.sidebar__list}>
         {sidebarRoutes.map((data, index) => {
           return (
