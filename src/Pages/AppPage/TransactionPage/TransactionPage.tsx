62541d64a8e2ab050b5f741850829a0579e185a2
Manoa <cmanoa92@gmail.com>
2022-10-17 13:25:44 +0300

    init transaction page

--- src/Pages/AppPage/TransactionPage/TransactionPage.tsx
+++ src/Pages/AppPage/TransactionPage/TransactionPage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./TransactionPage.module.scss";
 
 const TransactionPage = () => {
-  return <div>TransactionPage</div>;
+  return <div className={styles.transaction__head}></div>;
 };
 
 export default TransactionPage;
