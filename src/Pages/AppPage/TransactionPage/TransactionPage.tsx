8dc83d9f63ffe7fad717c8eb6a52a1afd524c251
Manoa <cmanoa92@gmail.com>
2022-10-17 14:14:31 +0300

    init transaction page

--- src/Pages/AppPage/TransactionPage/TransactionPage.tsx
+++ src/Pages/AppPage/TransactionPage/TransactionPage.tsx
@@ -1,8 +1,27 @@
+import { FormCustom } from "@/Components/DataEntry/FormCustom";
 import React from "react";
 import styles from "./TransactionPage.module.scss";
 
 const TransactionPage = () => {
-  return <div className={styles.transaction__head}></div>;
+  return (
+    <div className={styles.transaction__head}>
+      <FormCustom.SelectIcon
+        name="select"
+        icon="transactions"
+        placeholder="Transaction type"
+        options={[
+          {
+            label: "Be Network",
+            value: "Be Network",
+          },
+          {
+            label: "Bank Transfer",
+            value: "Be Network",
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default TransactionPage;
