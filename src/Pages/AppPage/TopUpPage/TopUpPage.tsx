5cbfa87906331a1d228cd2887caf15ccfc0c833f
Manoa <cmanoa92@gmail.com>
2022-10-19 18:39:18 +0300

    fix top up

--- src/Pages/AppPage/TopUpPage/TopUpPage.tsx
+++ src/Pages/AppPage/TopUpPage/TopUpPage.tsx
@@ -5,6 +5,7 @@ import TopUpPageNav from "./TopUpPageNav/TopUpPageNav";
 import styles from "./TopUpPage.module.scss";
 import CardConfirmItem from "@/Components/Display/CardConfirm/CardConfirmItem";
 import { useAppLayoutContext } from "@/Layouts/AppLayout/AppLayoutContext";
+import Icon from "@/Components/General/Icon/Icon";
 const moneyData = [
   {
     label: "USD (Mauritius)",
@@ -30,37 +31,78 @@ const moneyData = [
 const data = [
   {
     label: "Currency",
-    value: "USD",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        USD
+      </Text>
+    ),
   },
   {
     label: "Beneficiary Name",
-    value: "Be Mobile Limited",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        Be Mobile Limited
+      </Text>
+    ),
   },
   {
     label: "Beneficiary Address",
-    value:
-      "Level 2, Alexander House, Silicon Avenue,Cibercity, Ebene, Mauritius ",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        Level 2, Alexander House, Silicon Avenue,Cibercity, Ebene, Mauritius
+      </Text>
+    ),
   },
   {
     label: "Bank Name",
-    value: "The Mauritius Commercial Bank Ltd",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        The Mauritius Commercial Bank Ltd
+      </Text>
+    ),
   },
   {
     label: "Bank Address",
-    value: "9-15 Sir William Newton Street, Port Louis",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        9-15 Sir William Newton Street, Port Louis
+      </Text>
+    ),
   },
   {
     label: "Bank Country",
-    value: "Mauritius",
+    value: (
+      <Text tag="p" size={14} weight={600}>
+        Mauritius
+      </Text>
+    ),
   },
   {
     label: "Account Number",
-    value: "000449876447656",
+    value: (
+      <div className="flex items-center">
+        <Text tag="p" size={14} weight={600}>
+          000449876447656
+        </Text>
+        <Text tag="span" className="ml-4">
+          <Icon icon="copy" color="red" size={19} />
+        </Text>
+      </div>
+    ),
     icon: "bank-transfert",
   },
   {
     label: "IBAN",
-    value: "MU38MCBL876165318323290823132",
+    value: (
+      <div className="flex items-center">
+        <Text tag="p" size={14} weight={600}>
+          MU38MCBL876165318323290823132
+        </Text>
+        <Text tag="span" className="ml-4">
+          <Icon icon="copy" color="red" size={19} />
+        </Text>
+      </div>
+    ),
     icon: "bank-transfert",
   },
 ];
@@ -80,12 +122,7 @@ const TopUpPage = () => {
         ))}
       </div>
       {data.map((d, id) => (
-        <CardConfirmItem
-          label={d.label}
-          value={d.value}
-          icon={d.icon}
-          key={`d-${id}`}
-        />
+        <CardConfirmItem label={d.label} value={d.value} key={`d-${id}`} />
       ))}
     </Card>
   );
