370d0023a2d91fd2a6f308a22f49680fb8c157ba
Manoa Razanajatovo <cmanoa92@gmail.com>
2022-10-21 10:45:53 +0300

    overview transaction tab

--- src/Pages/AppPage/DashboardPage/DashboardPageTransaction.tsx
+++ src/Pages/AppPage/DashboardPage/DashboardPageTransaction.tsx
@@ -4,6 +4,7 @@ import CardTransaction, {
 } from "@/Components/Display/CardTransaction/CardTransaction";
 import Scrollbar from "@/Components/General/Scrollbar/Scrollbar";
 import Text from "@/Components/General/Text/Text";
+import { Tabs, TabsProps } from "antd";
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./DashboardPage.module.scss";
@@ -71,62 +72,59 @@ export default function DashboardPageTransaction() {
     }
   }, []);
 
+  const tabItems: TabsProps["items"] = paires.map((paire, index) => {
+    return {
+      label: paire,
+      key: `${index}`,
+      children: (
+        <div
+          style={{ "--offset-top": `${offsetTop}px` } as React.CSSProperties}
+          ref={psWrapRef}
+        >
+          <Card className={styles.transactionCard}>
+            <Scrollbar>
+              <div>
+                {transaction.map((t, index) => (
+                  <CardTransaction
+                    date={t.date}
+                    icon={t.icon}
+                    payment={t.payment}
+                    transaction={t.transaction}
+                    user={t.user}
+                    company={t.company}
+                    key={`t-${index}`}
+                  />
+                ))}
+              </div>
+            </Scrollbar>
+          </Card>
+        </div>
+      ),
+    };
+  });
+
   return (
     <div className={`${styles.dashboardTransaction}`}>
       <div className={styles.dashbaordTransactionHead}>
-        <Text tag="h2" type="h2" weight={600}>
+        <Text tag="h2" type="h2" variant="black2" weight={600}>
           Recent Transactions
         </Text>
-        <div className="flex justify-between">
-          <div className={styles.dashboardTransactionFilter}>
-            {paires.map((p, index) => (
-              <a
-                href=""
-                title=""
-                className={currentFilter === index ? styles.active : ""}
-                key={`p-${index}`}
-                onClick={(e) => handleFilter(e, index)}
-              >
-                <Text
-                  tag="span"
-                  variant={currentFilter === index ? "grey" : "grey-light"}
-                  weight={currentFilter === index ? 600 : 400}
-                >
-                  {p}
-                </Text>
-              </a>
-            ))}
-          </div>
-          <Link to="" title="">
-            <Text tag="span" variant="red" weight={500} size={14}>
-              View All
-            </Text>
-          </Link>
-        </div>
       </div>
-      <div
-        style={{ "--offset-top": `${offsetTop}px` } as React.CSSProperties}
-        ref={psWrapRef}
-        className="mt-4"
-      >
-        <Card className={styles.transactionCard}>
-          <Scrollbar>
-            <div>
-              {transaction.map((t, index) => (
-                <CardTransaction
-                  date={t.date}
-                  icon={t.icon}
-                  payment={t.payment}
-                  transaction={t.transaction}
-                  user={t.user}
-                  company={t.company}
-                  key={`t-${index}`}
-                />
-              ))}
-            </div>
-          </Scrollbar>
-        </Card>
-      </div>
+
+      <Tabs
+        tabBarStyle={{ marginBottom: 16, padding: "0 25px" }}
+        defaultActiveKey="1"
+        items={tabItems}
+        tabBarExtraContent={{
+          right: (
+            <Link to="" title="">
+              <Text tag="span" variant="red" weight={500} size={14}>
+                View All
+              </Text>
+            </Link>
+          ),
+        }}
+      />
     </div>
   );
 }
