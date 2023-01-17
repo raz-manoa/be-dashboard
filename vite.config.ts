73352e798544f039fb38dc98bf42ed2fb9db8e21
Oleksandr <osnisarenko@befinancialgroup.com>
2023-01-17 22:11:01 +0200

    try deploy to demo

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,22 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'static/js',
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: (assetInfo) => {
+    //       let extType = assetInfo.name.split('.').at(1);
+    //       if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+    //         extType = 'img';
+    //       }
+    //       return `assets/${extType}/[name]-[hash][extname]`;
+    //     },
+    //     chunkFileNames: 'static/js/[name]-[hash].js',
+    //     entryFileNames: 'static/js/[name]-[hash].js'
+    //   }
+    // }
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
