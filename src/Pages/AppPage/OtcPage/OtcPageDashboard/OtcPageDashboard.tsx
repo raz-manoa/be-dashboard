995aaa5d05709cb2b0b751964ab188fa6090171a
Manoa Razanajatovo <cmanoa92@gmail.com>
2023-01-18 09:59:40 +0300

    OTC fix

--- src/Pages/AppPage/OtcPage/OtcPageDashboard/OtcPageDashboard.tsx
+++ src/Pages/AppPage/OtcPage/OtcPageDashboard/OtcPageDashboard.tsx
@@ -2,14 +2,43 @@ import { useSetAppLayoutTitle } from "@/Layouts/AppLayout/AppLayoutContext";
 import { useNavigate } from "react-router-dom";
 import companyDataEndpoint, {
   AccountsResponse,
+  CurrencyInfo,
 } from "@/Api/endpoints/companyData.endpoint";
 import {
   ICardAmountForm,
   CardAmount,
 } from "@/Components/Display/CardAmount/CardAmount";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useOtcPagePageContext } from "../OtcPageContext";
+import { ECurrency } from "@/Interfaces/Currency";
+import Solana from "@/Assets/Logo/Solana.svg";
 
+const cryptoData: { [c in ECurrency]?: CurrencyInfo } = {
+  [ECurrency.USDC]: {
+    id: ECurrency.USDC,
+    sign: "USDC",
+    name: "USDC",
+    isCrypto: true,
+    precision: 8,
+    // icon: Solana,
+  },
+  [ECurrency.USD]: {
+    id: ECurrency.USD,
+    sign: "USD",
+    name: "USD",
+    isCrypto: true,
+    precision: 8,
+    // icon: ,
+  },
+  [ECurrency.ZAR]: {
+    id: ECurrency.ZAR,
+    sign: "ZAR",
+    name: "ZAR",
+    isCrypto: true,
+    precision: 8,
+    // icon: Zar,
+  },
+};
 const OtcPageDashboard = () => {
   useSetAppLayoutTitle("OTC");
 
@@ -38,11 +67,19 @@ const OtcPageDashboard = () => {
       .finally(() => setIsLoading(false));
   }, []);
 
+  const availableCurrency = useMemo(() => {
+    const availableSign: ECurrency[] = Object.keys(cryptoData) as ECurrency[];
+    return accounts.filter((account) =>
+      availableSign.includes(account.currency)
+    );
+  }, [cryptoData, accounts]);
+
   return (
     <CardAmount
       title="Amount"
-      selectFrom={accounts}
-      selectTo={accounts}
+      selectFrom={availableCurrency}
+      selectTo={availableCurrency}
+      allowSameCurrency={true}
       onSubmit={handleSubmit}
       loading={isLoading}
     />
